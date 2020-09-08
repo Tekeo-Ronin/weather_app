a3fb055506b7be5e9fca61901df3af0825555464
Володимир Коржевський <ulrichzven@gmail.com>
2020-09-08 12:02:24 +0300

    Подключил API

---
+++ js/main.js
@@ -0,0 +1,23 @@
+window.addEventListener('load', ()=> {
+  let long;
+  let lat;
+
+  if(navigator.geolocation){
+    navigator.geolocation.getCurrentPosition(position => {
+      long = position.coords.longitude;
+      lat = position.coords.latitude;
+
+      const proxy = `http://cors-anywhere.herokuapp.com/`;
+      const api = `${proxy}api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&APPID=31ff224de84100d105e67da0cf459f2c`;
+
+        
+      fetch(api)
+        .then(response => {
+          return response.json();
+        })
+        .then(data => {
+          console.log(data);
+        })
+    });
+  }
+});
