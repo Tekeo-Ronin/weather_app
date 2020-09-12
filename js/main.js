7f20072cbbb42f50c1c676265c9336dc4759ec48
Володимир Коржевський <ulrichzven@gmail.com>
2020-09-12 12:16:38 +0300

    test

--- js/main.js
+++ js/main.js
@@ -7,8 +7,8 @@ window.addEventListener('load', ()=> {
       long = position.coords.longitude;
       lat = position.coords.latitude;
 
-      const proxy = `http://cors-anywhere.herokuapp.com/`;
-      const api = `${proxy}api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&APPID=31ff224de84100d105e67da0cf459f2c`;
+      // const proxy = 'http://cors-anywhere.herokuapp.com/';
+      const api = `api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&APPID=31ff224de84100d105e67da0cf459f2c`;
 
         
       fetch(api)
