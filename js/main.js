5569631b5a6cc4832d69405fb4b6bf798012e5da
Володимир Коржевський <ulrichzven@gmail.com>
2020-09-14 21:50:59 +0300

    Прололжил подключать апи

--- js/main.js
+++ js/main.js
@@ -7,16 +7,16 @@ window.addEventListener('load', ()=> {
       long = position.coords.longitude;
       lat = position.coords.latitude;
 
-      // const proxy = 'http://cors-anywhere.herokuapp.com/';
-      const api = `api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&APPID=31ff224de84100d105e67da0cf459f2c`;
-
-        
+      const proxy = 'https://cors-anywhere.herokuapp.com/';
+      const api = `${proxy}https;//api.darksky.net/forecast/fd9d9c6418c23d94745b836767721ad1/${lat},${long}`;
+  
       fetch(api)
         .then(response => {
           return response.json();
         })
         .then(data => {
           console.log(data);
+          // const {temperature, summary} = data.currently; 
         })
     });
   }
