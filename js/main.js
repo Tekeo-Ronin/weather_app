612fe13d9b4800c10b4871661b0ff9cd68a235fb
Volodymyr Korzhevkiy <ulrichzven@gmail.com>
2020-10-20 03:30:55 +0300

    Completed connection API

--- js/main.js
+++ js/main.js
@@ -1,26 +1,42 @@
-window.addEventListener('load', ()=> {
+window.addEventListener("load", () => {
   let long;
   let lat;
-  let temperatureDescription = document.querySelector('.now-temperature_description');
-  let locationTimezone = document.querySelector('.now-timezone');
-  let temperatureDegree = document.querySelector('.now-temperature');
+  let temperatureDescription = document.querySelector(
+    ".now-temperature_description"
+  );
+  let locationTimezone = document.querySelector(".now-timezone");
+  let temperatureDegree = document.querySelector(".now-temperature");
 
-  if(navigator.geolocation){
-    navigator.geolocation.getCurrentPosition(position => {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition((position) => {
       long = position.coords.longitude;
       lat = position.coords.latitude;
 
-      const proxy = 'https://cors-anywhere.herokuapp.com/';
+      const proxy = "https://cors-anywhere.herokuapp.com/";
       const api = `${proxy}https://api.darksky.net/forecast/fd9d9c6418c23d94745b836767721ad1/${lat},${long}`;
-  
+
       fetch(api)
-        .then(response => {
+        .then((response) => {
           return response.json();
         })
-        .then(data => {
+        .then((data) => {
           console.log(data);
-          const {temperature, summary} = data.currently; 
-        })
+          const { temperature, summary, icon } = data.currently;
+
+          temperatureDegree.textContent =
+            Math.round((temperature - 32) * (5 / 9)) + "℃";
+          temperatureDescription.textContent = summary;
+          locationTimezone.textContent = data.timezone;
+
+          setIcons(icon, document.querySelector(".icon"));
+        });
     });
+  }
+
+  function setIcons(icon, iconID) {
+    const skycons = new Skycons({ color: "black" });
+    const currentIcon = icon.replace(/-/g, "_").toUpperCase();
+    skycons.play();
+    return skycons.set(iconID, Skycons[currentIcon]);
   }
 });
